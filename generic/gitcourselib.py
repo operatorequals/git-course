0861ee615e67e9b103787635c762565f71f03167
John Torakis <john.torakis@gmail.com>
2021-08-15 23:41:17 +0300

    [Generic] Add Code Checking in gitcourselib
    This commit adds several stuff in the library. Specifically, the
    `check_code` is implemented that accepts a commit and several
    keyphrases and ensures the contents of the commit patch contain the
    given keyphrases, in a per-file basis.
    
    This is a functionality already implemented in the previous
    challenges in a manual way, and now it is generalized and coded in
    the `gitcourselib`.
    
    Additioanlly, the `get_title_tag` extracts a commit tag (what is
    inside the `[]` and returns it.
    
    These values are used to identify commits that are supposed to
    contain specific code.

--- generic/gitcourselib.py
+++ generic/gitcourselib.py
@@ -40,3 +40,73 @@ def check_commit(commit):
 
   return True, f"[{commit.id}] Convention passes!"
 
+
+def get_title_tag(commit):
+  lines = commit.message.splitlines()
+  title = lines[0]
+  match_obj = re.search(r'\[(\w+)\].*', title)
+  try:
+    tag = match_obj[1]
+    return tag
+  except IndexError:
+    # Check if Merge commit with default message
+    if "Merge" in title:
+      return "Merge"
+    raise ValueError("Commit does not follow the '[Component] Title...' convention")
+
+
+def check_code(commit, code_snippets_to_add = {}, conflict_leftover_check=True):
+  '''
+  commit = pygit2 commit object
+  code_snippets_to_add = {'changed_filename': ["snippet_to_exist_1", "snippet_to_exist_2"]}
+  '''
+  tree = commit.tree
+  # Check against previous from the splitted commit
+  diff = tree.diff_to_tree(commit.parents[0].tree)
+  patches = [p for p in diff]
+
+  # Check the files changed in the commit
+  patch_files = {}
+  for p in patches:
+    if p.delta.new_file.path not in code_snippets_to_add:
+      return False, f"[{commit.id}] [{get_title_tag(commit)}] commit changes irrelevant file: '{p.delta.new_file.path}'"
+
+    patch_files[p.delta.new_file.path] = p
+
+  # Number of files changed different of files supposed to change
+  if len(patch_files.keys()) != len(code_snippets_to_add.keys()):
+    different_files = set(code_snippets_to_add.keys()) - set(patch_files.keys())
+    # Files that had to change but didn't
+    if different_files:
+      return False, f"[{commit.id}] [{get_title_tag(commit)}] commit did not change files '{different_files}'."
+
+    # Files that changed while they were not supposed to (also caught in the `patches` for-loop
+    different_files = set(patch_files.keys()) - set(code_snippets_to_add.keys())
+    if different_files:
+      return False, f"[{commit.id}] [{get_title_tag(commit)}] commit changes irrelevant files '{different_files}'."
+
+  # Iterate through all file changes
+  for patch_file, checked_patch in patch_files.items():
+    # Examine all commited hunks for lines
+    code = ""
+    for hunk in checked_patch.hunks:
+      for l in hunk.lines:
+        # print(l.origin, l.content, hunk)
+        if l.new_lineno == -1:
+
+          # print(l.origin, l.content, hunk)
+          code += l.content
+
+    # `code` contains all added code in the commit
+
+    # Check if all snippets are present in the code changes
+    for snippet in code_snippets_to_add[patch_file]:
+      if snippet not in code:
+        return False, f"[{commit.id}] [{get_title_tag(commit)}] commit does not seem to contain all the appropriate code."
+
+    if conflict_leftover_check:
+      for snippet in ["<<<<<<<", "=======", ">>>>>>>"]:
+        if snippet in code:
+          return False, f"[{commit.id}] [{get_title_tag(commit)}] commit seems to contain Git Conflict leftovers."
+
+  return True, f"[{commit.id}] [{get_title_tag(commit)}] commit passes!"
